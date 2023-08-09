519396df360a9b04b63a6c600f58f340fd6a1696
kselkami <99247829+kselkami@users.noreply.github.com>
2023-08-09 16:40:24 +0200

    1 file modified

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -1,18 +1,27 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 
 const Banner = () => {
-    const [isShowing, setIsShowing] = useState(true);
+  const [isShowing, setIsShowing] = useState(true);
 
-    function toggleShow() {
-        setIsShowing(false);
-    }
+  function toggleShow() {
+    setIsShowing(false);
+  }
 
   return (
-    <section className={`relative w-full h-[2rem] bg-black flex items-center justify-center text-white ${isShowing ? '' : 'hidden'}`}>
-        <span>Blah blah blah buy now for 50% off</span>
-        <button onClick={toggleShow} className='absolute right-4 top-[50%] translate-y-[-50%]'>x</button>
-      </section>
-  )
-}
+    <section
+      className={`relative w-full h-[2rem] bg-black flex items-center justify-center text-white ${
+        isShowing ? '' : 'hidden'
+      }`}
+    >
+      <span>Blah blah blah buy now for 50% off</span>
+      <button
+        onClick={toggleShow}
+        className="absolute p-2 py-[1px] rounded-md bg-white text-black right-4 top-[50%] translate-y-[-50%]"
+      >
+        x
+      </button>
+    </section>
+  );
+};
 
-export default Banner
+export default Banner;
